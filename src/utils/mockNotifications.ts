d4ba41c608fbe9be76b691ea4f292e516f749e9b
Luciano Montilla <unonueve95@gmail.com>
2024-11-28 13:09:59 -0300

    Add mock data

---
+++ src/utils/mockNotifications.ts
@@ -0,0 +1,50 @@
+export const notis = [
+    {
+        _id: "0120ff12013g34",
+        orderToken: "530013",
+        customer: "Bobby Briggs",
+        action: "canceló",
+    },
+    {
+        _id: "0120ff12013joi",
+        orderToken: "530014",
+        customer: "Audrey Horne",
+        action: "compró",
+        item: "Jueguete Interactivo",
+        quantity: 3,
+    },
+    {
+        _id: "0120ff12013j87",
+        orderToken: "530015",
+        customer: "Dale Cooper",
+        action: "compró",
+        item: "Rascador de Cartón",
+        quantity: 1,
+    },
+    {
+        _id: "0120ff12013012",
+        orderToken: "530015",
+        customer: "Dale Cooper",
+        action: "pagó",
+    },
+    {
+        _id: "0120ff12013fi9",
+        orderToken: "530015",
+        customer: "Dale Cooper",
+        action: "retiró",
+    },
+    {
+        _id: "0120ff12013fj2",
+        orderToken: "530016",
+        customer: "Laura Palmer",
+        action: "compró",
+        item: "Cama para Gatos",
+        quantity: 1,
+    },
+    {
+        _id: "0120ff12013fj2",
+        orderToken: "530016",
+        customer: "Laura Palmer",
+        action: "pagó",
+    },
+];
---
+++ src/utils/mockOrders.ts
@@ -0,0 +1,32 @@
+export const orders = [
+    {
+        _id: "a89svga99f7y",
+        orderToken: "530013",
+        item: "Comedero Doble",
+        quantity: 1,
+        paymentStatus: "Cancelado",
+    },
+    {
+        _id: "a89svga994t6",
+        orderToken: "530014",
+        item: "Jueguete Interactivo",
+        quantity: 3,
+        paymentStatus: "Pago pendiente",
+    },
+    {
+        _id: "a89svga99h89",
+        orderToken: "530015",
+        item: "Rascador de Cartón",
+        quantity: 1,
+        paymentStatus: "Pagado",
+        deliveryStatus: "Entregado",
+    },
+    {
+        _id: "a89svga9978a",
+        orderToken: "530016",
+        item: "Cama para Gatos",
+        quantity: 1,
+        paymentStatus: "Pagado",
+        deliveryStatus: "Entrega pendiente",
+    },
+];
